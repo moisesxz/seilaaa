fa9a4443ea761d490b33b528c2eb1d0637a35b82
Moises Santos <Moises Santos@DESKTOP-OCUQ1EB>
2020-03-01 18:18:18 -0300

    fiz alteração

--- src/trabalho1/Trabalho1.java
+++ src/trabalho1/Trabalho1.java
@@ -15,6 +15,8 @@ public class Trabalho1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+    System.out.println("eai");    
         // TODO code application logic here
     }
     
